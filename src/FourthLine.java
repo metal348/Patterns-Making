906feec561f54226bda082b9aed4b69e279aecf8
metal348 <malavgandhi_13@outlook.com>
2023-02-18 20:18:09 +0530

    New Idea

---
+++ src/FourthLine.java
@@ -0,0 +1,169 @@
+class FourthLine{
+    FourthLine(String input){
+        for(int i = 0; i<=input.length()-1;i++){
+            char c = input.charAt(i);
+            switch(c){
+                case 'A':
+                displayA(c);
+                break;
+                case 'B':
+                displayB(c);
+                break;
+                case 'C':
+                displayC(c);
+                break;
+                case 'D':
+                displayD(c);
+                break;
+                case 'E':
+                displayE(c);
+                break;
+                case 'F':
+                displayF(c);
+                break;
+                case 'G':
+                displayG(c);
+                break;
+                case 'H':
+                displayH(c);
+                break;
+                case 'I':
+                displayI(c);
+                break;
+                case 'J':
+                displayJ(c);
+                break;
+                case 'K':
+                displayK(c);
+                break;
+                case 'L':
+                displayL(c);
+                break;
+                case 'M':
+                displayM(c);
+                break;
+                case 'N':
+                displayN(c);
+                break;
+                case 'O':
+                displayO(c);
+                break;
+                case 'P':
+                displayP(c);
+                break;
+                case 'Q':
+                displayQ(c);
+                break;
+                case 'R':
+                displayR(c);
+                break;
+                case 'S':
+                displayS(c);
+                break;
+                case 'T':
+                displayT(c);
+                break;
+                case 'U':
+                displayU(c);
+                break;
+                case 'V':
+                displayV(c);
+                break;
+                case 'X':
+                displayX(c);
+                break;
+                case 'Y':
+                displayY(c);
+                break;
+                case 'Z':
+                displayZ(c);
+                break;
+    
+    
+                
+            }
+        }
+    }
+    void displayA(char c){
+    
+    }
+    void displayB(char c){
+        
+    }
+    void displayC(char c){
+        
+    }
+    void displayD(char c){
+        
+    }
+    
+    void displayE(char c){
+        
+    }
+    void displayF(char c){
+        System.out.print("* * * * * *  ");
+    }
+    
+    
+    void displayG(char c){
+        
+    }
+    void displayH(char c){
+        
+    }
+    void displayI(char c){
+        
+    }
+    void displayJ(char c){
+        
+    }
+    void displayK(char c){
+        
+    }
+    void displayL(char c){
+        
+    }
+    void displayM(char c){
+        
+    }
+    void displayN(char c){
+        
+    }
+    void displayO(char c){
+        
+    }
+    void displayP(char c){
+        
+    }
+    void displayQ(char c){
+        
+    }
+    void displayR(char c){
+        
+    }
+    void displayS(char c){
+        
+    }
+    void displayT(char c){
+        
+    }
+    void displayU(char c){
+        
+    }
+    void displayV(char c){
+        
+    }
+    void displayW(char c){
+        
+    }
+    void displayX(char c){
+        
+    }
+    void displayY(char c){
+        
+    }
+    void displayZ(char c){
+        
+    }
+    
+}
--- src/Main5.java
+++ src/Main5.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 class Main5{
@@ -5,89 +6,19 @@ class Main5{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
         String input = sc.nextLine();
-        for(int i = 0; i<=input.length()-1;i++){
-            char c = input.charAt(i);
-            AllPattern a1 = new AllPattern();
-            switch(c){
-                case 'A':
-                a1.displayA(c);
-                break;
-                case 'B':
-                a1.displayB(c);
-                break;
-                case 'C':
-                a1.displayC(c);
-                break;
-                case 'D':
-                a1.displayD(c);
-                break;
-                case 'E':
-                a1.displayE(c);
-                break;
-                case 'F':
-                a1.displayF(c);
-                break;
-                case 'G':
-                a1.displayG(c);
-                break;
-                case 'H':
-                a1.displayH(c);
-                break;
-                case 'I':
-                a1.displayI(c);
-                break;
-                case 'J':
-                a1.displayJ(c);
-                break;
-                case 'K':
-                a1.displayK(c);
-                break;
-                case 'L':
-                a1.displayL(c);
-                break;
-                case 'M':
-                a1.displayM(c);
-                break;
-                case 'N':
-                a1.displayN(c);
-                break;
-                case 'O':
-                a1.displayO(c);
-                break;
-                case 'P':
-                a1.displayP(c);
-                break;
-                case 'Q':
-                a1.displayQ(c);
-                break;
-                case 'R':
-                a1.displayR(c);
-                break;
-                case 'S':
-                a1.displayS(c);
-                break;
-                case 'T':
-                a1.displayT(c);
-                break;
-                case 'U':
-                a1.displayU(c);
-                break;
-                case 'V':
-                a1.displayV(c);
-                break;
-                case 'X':
-                a1.displayX(c);
-                break;
-                case 'Y':
-                a1.displayY(c);
-                break;
-                case 'Z':
-                a1.displayZ(c);
-                break;
-
-
-                
-            }
-        }
+        FirstLine f1 = new FirstLine(input);
+        System.out.println();
+        SecondLine f2 = new SecondLine(input);
+        System.out.println();
+        ThirdLine f3 = new ThirdLine(input);
+        System.out.println();
+        FourthLine f4 = new FourthLine(input);
+        System.out.println();
+        FifthLine f5 = new FifthLine(input);
+        System.out.println();
+        SixthLine f6 = new SixthLine(input);
+        System.out.println();
+        LastLine f7 = new LastLine(input);
+        System.out.println();
     }
 }
