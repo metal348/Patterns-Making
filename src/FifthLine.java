4d389f9513776af4f4cdb61e3044e4f90b39c321
Malav Gandhi <119118965+metal348@users.noreply.github.com>
2023-03-05 15:44:20 +0530

    Update FifthLine.java

--- src/FifthLine.java
+++ src/FifthLine.java
@@ -1,168 +1,169 @@
-class FifthLine{
-    FifthLine(String input){
-        for(int i = 0; i<=input.length()-1;i++){
-            char c = input.charAt(i);
-            switch(c){
-                case 'A':
-                displayA(c);
-                break;
-                case 'B':
-                displayB(c);
-                break;
-                case 'C':
-                displayC(c);
-                break;
-                case 'D':
-                displayD(c);
-                break;
-                case 'E':
-                displayE(c);
-                break;
-                case 'F':
-                displayF(c);
-                break;
-                case 'G':
-                displayG(c);
-                break;
-                case 'H':
-                displayH(c);
-                break;
-                case 'I':
-                displayI(c);
-                break;
-                case 'J':
-                displayJ(c);
-                break;
-                case 'K':
-                displayK(c);
-                break;
-                case 'L':
-                displayL(c);
-                break;
-                case 'M':
-                displayM(c);
-                break;
-                case 'N':
-                displayN(c);
-                break;
-                case 'O':
-                displayO(c);
-                break;
-                case 'P':
-                displayP(c);
-                break;
-                case 'Q':
-                displayQ(c);
-                break;
-                case 'R':
-                displayR(c);
-                break;
-                case 'S':
-                displayS(c);
-                break;
-                case 'T':
-                displayT(c);
-                break;
-                case 'U':
-                displayU(c);
-                break;
-                case 'V':
-                displayV(c);
-                break;
-                case 'X':
-                displayX(c);
-                break;
-                case 'Y':
-                displayY(c);
-                break;
-                case 'Z':
-                displayZ(c);
-                break;
-    
-    
-                
+    class FifthLine{
+        FifthLine(String input){
+            for(int i = 0; i<=input.length()-1;i++){
+                char c = input.charAt(i);
+                switch(c){
+                    case 'A':
+                    displayA(c);
+                    break;
+                    case 'B':
+                    displayB(c);
+                    break;
+                    case 'C':
+                    displayC(c);
+                    break;
+                    case 'D':
+                    displayD(c);
+                    break;
+                    case 'E':
+                    displayE(c);
+                    break;
+                    case 'F':
+                    displayF(c);
+                    break;
+                    case 'G':
+                    displayG(c);
+                    break;
+                    case 'H':
+                    displayH(c);
+                    break;
+                    case 'I':
+                    displayI(c);
+                    break;
+                    case 'J':
+                    displayJ(c);
+                    break;
+                    case 'K':
+                    displayK(c);
+                    break;
+                    case 'L':
+                    displayL(c);
+                    break;
+                    case 'M':
+                    displayM(c);
+                    break;
+                    case 'N':
+                    displayN(c);
+                    break;
+                    case 'O':
+                    displayO(c);
+                    break;
+                    case 'P':
+                    displayP(c);
+                    break;
+                    case 'Q':
+                    displayQ(c);
+                    break;
+                    case 'R':
+                    displayR(c);
+                    break;
+                    case 'S':
+                    displayS(c);
+                    break;
+                    case 'T':
+                    displayT(c);
+                    break;
+                    case 'U':
+                    displayU(c);
+                    break;
+                    case 'V':
+                    displayV(c);
+                    break;
+                    case 'W':
+                        displayW(c);
+                        break;
+                    case 'X':
+                    displayX(c);
+                    break;
+                    case 'Y':
+                    displayY(c);
+                    break;
+                    case 'Z':
+                    displayZ(c);
+                    break;
+        
+        
+                    
+                }
             }
         }
-    }
+        
+        void displayA(char c){ System.out.print("  * * * * *    "); }
+        void displayB(char c){
+            System.out.print("*         *    ");
+        }
+        void displayC(char c){
+            System.out.print("*              ");
+        }
+        void displayD(char c){
+            System.out.print("*         *    ");
+        }
+        
+        void displayE(char c){
+            System.out.print("*            ");
+        }
+        void displayF(char c){
+            System.out.print("*            ");
+        }
+        void displayG(char c){
+            System.out.print("*     *  *       ");
+        }
+        void displayH(char c){
+            System.out.print("*       *    ");
+        }
+        void displayI(char c){
+            System.out.print("    *        ");
+        }
+        void displayJ(char c){
+            System.out.print("      *        ");
+        }
+        void displayK(char c){
+            System.out.print("* *        ");
+        }
+        void displayL(char c){
+            System.out.print("*            ");
+        }
+        void displayM(char c){
+            System.out.print("*           *    ");
+        }
+        void displayN(char c){
+            System.out.print("*       *   *    ");
+        }
+        void displayO(char c){
+            System.out.print("*           *    ");
+        }
+        void displayP(char c){
+            System.out.print("*            ");
+        }
+        void displayQ(char c){
+            System.out.print("*       *   *    ");
+        }
+        void displayR(char c){
+            System.out.print("*  *         ");
+        }
+        void displayS(char c){
+            System.out.print("        *    ");
+        }
+        void displayT(char c){
+            System.out.print("    *        ");
+        }
+        void displayU(char c){
+            System.out.print("*       *    ");
+        }
+        void displayV(char c){
+            System.out.print("    *   *       ");
+        }
+        void displayW(char c){
+            System.out.print("    *   * *   *       ");
+        }
+        void displayX(char c){
+            System.out.print("  * *      ");
+        }
+        void displayY(char c){
+            System.out.print("    *        ");
+        }
+        void displayZ(char c){
+            System.out.print("  *        ");
+        }
     
-    void displayA(char c){
-    
     }
-    void displayB(char c){
-        
-    }
-    void displayC(char c){
-        
-    }
-    void displayD(char c){
-        
-    }
-    
-    void displayE(char c){
-        
-    }
-    void displayF(char c){
-        System.out.print("*            ");
-    }
-    void displayG(char c){
-        
-    }
-    void displayH(char c){
-        
-    }
-    void displayI(char c){
-        
-    }
-    void displayJ(char c){
-        
-    }
-    void displayK(char c){
-        
-    }
-    void displayL(char c){
-        
-    }
-    void displayM(char c){
-        
-    }
-    void displayN(char c){
-        
-    }
-    void displayO(char c){
-        
-    }
-    void displayP(char c){
-        
-    }
-    void displayQ(char c){
-        
-    }
-    void displayR(char c){
-        
-    }
-    void displayS(char c){
-        
-    }
-    void displayT(char c){
-        
-    }
-    void displayU(char c){
-        
-    }
-    void displayV(char c){
-        
-    }
-    void displayW(char c){
-        
-    }
-    void displayX(char c){
-        
-    }
-    void displayY(char c){
-        
-    }
-    void displayZ(char c){
-        
-    }
-   
-}
